398d2cb4801658ce19a81dd1e4e017cb1e0df189
Felipe Navas <felipenavas.itec@gmail.com>
2022-08-23 09:00:36 -0300

    Adding actions in reducer

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -2,25 +2,49 @@ import { useReducer } from 'react'
 import { todoReducer } from './todoReducer'
 import { TodoList } from './TodoList'
 import { TodoAdd } from './TodoAdd'
+import { useEffect } from 'react'
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: 'Buy milk',
-    done: false,
-  },
-  {
-    id: new Date().getTime() + 100,
-    description: 'Do homework',
-    done: false,
-  },
+  // {
+  //   id: new Date().getTime(),
+  //   description: 'Buy milk',
+  //   done: false,
+  // },
 ]
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState)
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const handleNewTodo = (newTodo) => {
-    console.log(newTodo)
+    const action = {
+      type: '[TODO] Add',
+      payload: newTodo,
+    }
+    dispatch(action)
+  }
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: '[TODO] Delete',
+      payload: id,
+    }
+    dispatch(action)
+  }
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: '[TODO] Toggle',
+      payload: id,
+    }
+    dispatch(action)
   }
 
   return (
@@ -32,7 +56,11 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
 
         <div className="col-5">
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,7 +1,15 @@
 export const todoReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ABC':
-      throw new Error('Action.type ABC is not supported')
+    case '[TODO] Add':
+      return [...state, action.payload]
+
+    case '[TODO] Delete':
+      return state.filter((todo) => todo.id !== action.payload)
+
+    case '[TODO] Toggle':
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      )
 
     default:
       return state
