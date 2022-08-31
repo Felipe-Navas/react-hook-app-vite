96e948591a097c8cf64460d19bfb73a33828f05c
Felipe Navas <felipenavas.itec@gmail.com>
2022-08-31 08:57:46 -0300

    Working with useContext and router

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,27 @@
+import { useContext } from 'react'
+import { UserContext } from './context/UserContext'
+
 export const LoginPage = () => {
+  const { user, setUser } = useContext(UserContext)
+
+  const loginUser = () => {
+    setUser({
+      id: 123,
+      name: 'Don nadie',
+      email: 'donNadie@gmail.com',
+    })
+  }
+
   return (
     <>
       <h1>LoginPage</h1>
       <hr />
+
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+
+      <button className="btn btn-primary" onClick={loginUser}>
+        Login User
+      </button>
     </>
   )
 }
