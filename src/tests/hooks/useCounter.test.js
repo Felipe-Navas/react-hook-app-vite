d1a9f6df09224906adbc3e3607572fb731687caa
Felipe Navas <felipenavas.itec@gmail.com>
2024-04-26 21:30:27 -0300

    Update package.json and tests

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,4 +1,5 @@
-import { act, renderHook } from '@testing-library/react'
+import { act } from 'react'
+import { renderHook } from '@testing-library/react'
 import { useCounter } from '../../hooks/useCounter'
 
 describe('Testing the useCounter', () => {
